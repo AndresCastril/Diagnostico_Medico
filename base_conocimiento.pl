4c6b9610a909f87abadf6cd5bd84e09fea1c69b0
Andres54 <andrescastrillon2005@gmail.com>
2025-12-09 00:03:20 -0500

    Se inició la base de conocimiento

--- base_conocimiento.pl
+++ base_conocimiento.pl
@@ -1,3 +1,12 @@
-% Hecho de prueba
-saludo(hola).
-saludo(mundo).
+% Hechos dynamic
+
+:- dynamic enfermedad/3.        % (nombre, categoria, recomendacion)
+:- dynamic es_sintoma/1.        % (nombre)
+:- dynamic tiene_sintoma/2.     % (enfermedad, sintoma)
+:- dynamic paciente/2.          % (nombre, edad)
+:- dynamic historial/3.         % (paciente, enfermedad, fecha)
+
+% Reglas estáticas
+
+recomendacion(Enfermedad, Recomendacion) :- enfermedad(Enfermedad, _, Recomendacion).
+
